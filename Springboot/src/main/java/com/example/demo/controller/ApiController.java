2dc20063501aa600a1c004cec1b2daf9c8185e4c
teacher <teacher@DESKTOP-TDDQARH>
2025-08-11 18:48:21 +0800

    web

--- Springboot/src/main/java/com/example/demo/controller/ApiController.java
+++ Springboot/src/main/java/com/example/demo/controller/ApiController.java
@@ -5,13 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 了解各種不同 URL 與參數的傳遞與接收
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 	
-	@GetMapping("/hello")
+	/**
+	 * 1. 歡迎頁
+	 * 路徑:/hello
+	 * 路徑:/home
+	 * 路徑:/welcome
+	 * 網址:http://localhost:8080/api/hello
+	 * 網址:http://localhost:8080/api/home
+	 * 網址:http://localhost:8080/api/welcome
+	 * */
+	//@GetMapping("/hello")
+	@GetMapping(value = {"/hello", "/home", "/welcome"})
 	public String hello() {
 		return "Hello Spring " + new Date();
 	}
+	
+	
 	
 }
