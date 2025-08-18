a1de7a31e9f7b172ce845c247279dd03fdf8976d
teacher <teacher@DESKTOP-TDDQARH>
2025-08-18 20:34:44 +0800

    web

---
+++ Springboot/src/main/java/com/example/demo/service/impl/ParkingSlotServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.bean.ParkingSlot;
+import com.example.demo.service.ParkingSlotService;
+
+@Service
+public class ParkingSlotServiceImpl implements ParkingSlotService {
+
+	@Override
+	public List<ParkingSlot> getAllSlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String parkCar(int slot, String plate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String leaveCar(int slot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String addSlot(int slot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteSlot(int slot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
