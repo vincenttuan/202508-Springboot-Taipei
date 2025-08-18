a7c8f92fa0582f21b395ab24818538a39afe9a7f
teacher <teacher@DESKTOP-TDDQARH>
2025-08-18 20:42:16 +0800

    web

--- Springboot/src/main/java/com/example/demo/service/impl/ParkingSlotServiceImpl.java
+++ Springboot/src/main/java/com/example/demo/service/impl/ParkingSlotServiceImpl.java
@@ -2,30 +2,42 @@ package com.example.demo.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.ParkingSlotDao;
 import com.example.demo.model.bean.ParkingSlot;
 import com.example.demo.service.ParkingSlotService;
 
 @Service
 public class ParkingSlotServiceImpl implements ParkingSlotService {
-
+	
+	@Autowired
+	private ParkingSlotDao dao;
+	
 	@Override
 	public List<ParkingSlot> getAllSlots() {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.findAll();
 	}
 
 	@Override
 	public String parkCar(int slot, String plate) {
-		// TODO Auto-generated method stub
-		return null;
+		boolean check = dao.parkCar(slot, plate);
+		if(check) {
+			return String.format("車牌 %s 已停入 %d 號車位", plate, slot);
+		} else {
+			return String.format("車牌 %s 無法停入 %d 號車位", plate, slot);
+		}
 	}
 
 	@Override
 	public String leaveCar(int slot) {
-		// TODO Auto-generated method stub
-		return null;
+		boolean check = dao.leaveCar(slot);
+		if(check) {
+			return String.format("%d 號車位的車子已經離開", slot);
+		} else {
+			return String.format("%d 號車位目前無車", slot);
+		}
 	}
 
 	@Override
