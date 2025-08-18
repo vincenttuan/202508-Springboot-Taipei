6fbdef34814d7180dd21c7a160317b185645f2aa
teacher <teacher@DESKTOP-TDDQARH>
2025-08-18 19:29:17 +0800

    web

---
+++ Springboot/src/main/java/com/example/demo/model/bean/ParkingSlot.java
@@ -0,0 +1,26 @@
+package com.example.demo.model.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/*
+use web;
+create table if not exists parking_slot(
+	slot int primary key,
+    plate varchar(50) not null default ""
+);
+
+-- 初始化  1~5 號空車位
+insert into parking_slot (slot, plate) values (1, ''), (2, ''), (3, ''), (4, ''), (5, '')
+*/
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkingSlot {
+	private Integer slot;
+	private String plate;
+}
