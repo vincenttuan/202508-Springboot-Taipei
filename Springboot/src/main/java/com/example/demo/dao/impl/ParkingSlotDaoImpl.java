ab51eb64bb4fce38db4266b12eaebae8e0e3a7d5
teacher <teacher@DESKTOP-TDDQARH>
2025-08-18 19:45:29 +0800

    web

--- Springboot/src/main/java/com/example/demo/dao/impl/ParkingSlotDaoImpl.java
+++ Springboot/src/main/java/com/example/demo/dao/impl/ParkingSlotDaoImpl.java
@@ -2,6 +2,9 @@ package com.example.demo.dao.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.dao.ParkingSlotDao;
@@ -9,11 +12,14 @@ import com.example.demo.model.bean.ParkingSlot;
 
 @Repository
 public class ParkingSlotDaoImpl implements ParkingSlotDao {
-
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public List<ParkingSlot> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select slot, plate from parking_slot order by slot";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ParkingSlot.class));
 	}
 
 	@Override
