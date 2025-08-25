68687f370c3ba533552a83c57c378482adaf6c75
teacher <teacher@DESKTOP-TDDQARH>
2025-08-25 19:10:57 +0800

    r

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -8,10 +8,11 @@ import App4 from './App4.jsx'
 import App5 from './App5.jsx'
 import App6 from './App6.jsx'
 import App7 from './App7.jsx'
+import App8 from './App8.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App7 />
+    <App8 />
   </React.StrictMode>,
 )
