22d447e10732a11c8ee3958d887b944062d0fc63
teacher <teacher@DESKTOP-TDDQARH>
2025-08-25 20:00:27 +0800

    web

--- my-react/src/App10.jsx
+++ my-react/src/App10.jsx
@@ -10,8 +10,42 @@ function App10() {
     // 以 <table>印出
     // 學號(id) 姓名(name) 國文(chinese) 數學(math) 英文(english) 個人平均
     // 最後印出全班平均
-    
 
+    // 全班平均
+    const classAvg = students.reduce((sum, student) => 
+        sum + (student.scores.chinese + student.scores.math + student.scores.english)/3, 0) / students.length;
+
+    return(
+        <>
+            <h1>期末成績</h1>
+            <h2>班平均: {classAvg.toFixed(1)}</h2>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>學號(id)</th><th>姓名(name)</th><th>國文(chinese)</th>
+                        <th>數學(math)</th><th>英文(english)</th><th>個人平均</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        students.map((student) => {
+                            const avg = (student.scores.chinese + student.scores.math + student.scores.english)/3;
+                            return (
+                                <tr key={student.id}>
+                                    <td>{student.id}</td>
+                                    <td>{student.name}</td>
+                                    <td>{student.scores.chinese}</td>
+                                    <td>{student.scores.math}</td>
+                                    <td>{student.scores.english}</td>
+                                    <td>{avg.toFixed(1)}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default App10;
