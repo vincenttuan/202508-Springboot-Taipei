7926a7e79e166bfe0b86093806fd2f60e13083a5
teacher <teacher@DESKTOP-TDDQARH>
2025-08-25 19:43:06 +0800

    web

---
+++ my-react/src/App10.jsx
@@ -0,0 +1,17 @@
+function App10() {
+    // 全班成績
+    const students = [
+        {id:1, name:'小明', scores: {chinese: 80, math: 70, english: 90}},
+        {id:2, name:'小華', scores: {chinese: 60, math: 85, english: 75}},
+        {id:3, name:'小美', scores: {chinese: 95, math: 88, english: 92}},
+        {id:4, name:'小強', scores: {chinese: 70, math: 65, english: 80}}
+    ];
+
+    // 以 <table>印出
+    // 學號(id) 姓名(name) 國文(chinese) 數學(math) 英文(english) 個人平均
+    // 最後印出全班平均
+    
+
+}
+
+export default App10;
