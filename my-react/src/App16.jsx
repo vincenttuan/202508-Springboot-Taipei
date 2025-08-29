516abc70d642fa8c55dd5dbf33a01a20ca1899af
teacher <teacher@DESKTOP-TDDQARH>
2025-08-29 19:21:40 +0800

    react

--- my-react/src/App16.jsx
+++ my-react/src/App16.jsx
@@ -5,16 +5,34 @@
  * 要顯示目前所喝的水量與目標量
  * 
  */
+import { useState } from "react";
 
 function App16() {
+    const maxCC = 3000; // 目標 cc
+    const cupCC = 250; // 一杯 cc
+    const [cups, setCups] = useState(0); // 紀錄喝了幾杯
+
+    // 已喝水量
+    const drankCC = cups * cupCC;
+
+    // 喝水(每次一杯)
+    function drinkWater() {
+        setCups(cups + 1);
+    }
+
+    // 清除
+    function reset() {
+        setCups(0);
+    }
+
     return(
         <>
             <h1>飲水紀錄</h1>
             <div>
-                已喝了 0 / 3000 cc
+                已喝了 {drankCC} / {maxCC} cc
             </div>
-            <button>喝一杯水 (250cc)</button>
-            <button>清除記錄 (歸 0)</button>
+            <button onClick={drinkWater}>喝一杯水 (250cc)</button>
+            <button onClick={reset}>清除記錄 (歸 0)</button>
         </>
     )
 }
