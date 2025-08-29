f1caa27cdf499e8c9b568eb51f5d3b6f949b4229
teacher <teacher@DESKTOP-TDDQARH>
2025-08-29 20:39:04 +0800

    react

--- my-react/src/App17.jsx
+++ my-react/src/App17.jsx
@@ -6,7 +6,44 @@
  * 記錄吃了幾種食物並計算大卡
  */
 
+import { useState } from "react";
+
 function App17() {
+    // 定義食物及熱量
+    const food1 = { name: "餅乾", calories: 150 };
+    const food2 = { name: "蛋糕", calories: 300 };
+    const food3 = { name: "水果", calories: 80 };
+
+    // 利用 useState 來記錄每種食物的份數 
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [count3, setCount3] = useState(0);
+    
+    // 總熱量
+    const [calories, setCalories] = useState(0);
+    
+    // 吃指定食物
+    function eatFood(foodNumber) {
+        switch(foodNumber) {
+            case 1: // 餅乾
+                setCount1(count1 + 1);
+                setCalories(calories + food1.calories);
+            case 2: // 蛋糕
+                setCount2(count2 + 1);
+                setCalories(calories + food2.calories);
+            case 3: // 水果
+                setCount3(count3 + 1);
+                setCalories(calories + food3.calories);
+        }
+    }
+
+    function reset() {
+        setCount1(0);
+        setCount2(0);
+        setCount3(0);
+        setCalories(0);
+    }
+
     return(
         <>
             <h1>多樣點心模擬器</h1>
