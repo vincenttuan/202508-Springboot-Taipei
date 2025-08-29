d9044f64a1f4880099dc9a1f5a8e490d98ea9ccb
teacher <teacher@DESKTOP-TDDQARH>
2025-08-29 21:09:52 +0800

    react

--- my-react/src/App18.jsx
+++ my-react/src/App18.jsx
@@ -9,22 +9,58 @@
 import { useState } from "react";
 
 function App18() {
-    // 定義食物及熱量
-    const food1 = { name: "餅乾", calories: 150 };
-    const food2 = { name: "蛋糕", calories: 300 };
-    const food3 = { name: "水果", calories: 80 };
+    // 食物陣列(定義食物及熱量)
+    const foods = [
+        { name: "餅乾", calories: 150 },
+        { name: "蛋糕", calories: 300 },
+        { name: "水果", calories: 80 }
+        
+    ];
+
+    // 利用 useState 來記錄每種食物的份數
+    //const [counts, setCounts] = useState([0, 0, 0]);
+    const [counts, setCounts] = useState(Array(foods.length).fill(0));
+
+    // 總熱量
+    const [calories, setCalories] = useState(0);
+
+    // 吃指定食物
+    function eatFood(i) {
+        // 取得 counts 陣列資料
+        //const currentCounts = Array.from(counts);
+        const currentCounts = [...counts];
+        currentCounts[i] += 1;
+
+        setCounts(currentCounts);
+        setCalories(calories + foods[i].calories);
+    }
+
+    // 清除/重置
+    function reset() {
+        setCounts(Array(foods.length).fill(0));
+        setCalories(0);
+    }
 
     
     return(
         <>
             <h1>多樣點心模擬器</h1>
-            <h2>攝取熱量: 0 卡</h2>
+            <h2>攝取熱量: {calories} 卡</h2>
             <ul>
-                <li>xx : 份</li>
+                {
+                    foods.map((food, i) => (
+                        <li key={i}>{food.name} : {counts[i]} 份</li>
+                    ))
+                }
             </ul>
             <p />
-            <button>吃xx (+0卡)</button>
-            <button>清除 (歸 0)</button>
+            {
+                foods.map((food, i) => (
+                    <button onClick={() => eatFood(i)}>吃{food.name} (+{food.calories}卡)</button>
+                ))
+            }
+            
+            <button onClick={reset}>清除 (歸 0)</button>
         </>
     )
 }
