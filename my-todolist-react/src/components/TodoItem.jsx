373d00427b5a7abb184fccd78506dbb5479dbd87
teacher <teacher@DESKTOP-TDDQARH>
2025-09-05 21:21:57 +0800

    react

--- my-todolist-react/src/components/TodoItem.jsx
+++ my-todolist-react/src/components/TodoItem.jsx
@@ -1,9 +1,13 @@
 function TodoItem({index, todo, changeCompleted}) {
 
     return(
-        <li key={todo.id}>
+        <li key={todo.id}
+            className={`list-group-item d-flex justify-content-between align-items-center ${
+                todo.completed ? 'list-group-item-success' : ''
+            }`}
+        >
             {index} - {todo.id} - {todo.text} - 
-            <input type="checkbox" checked={todo.completed} onChange={() => changeCompleted(todo.id)} />
+            <input className="me-2" type="checkbox" checked={todo.completed} onChange={() => changeCompleted(todo.id)} />
         </li>
     )
 
