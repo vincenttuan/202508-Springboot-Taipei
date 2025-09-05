3ea2e849e9e2b3b42a737cb054a05bc389988b12
teacher <teacher@DESKTOP-TDDQARH>
2025-09-05 19:09:31 +0800

    web

---
+++ my-todolist-react/src/App2.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+// 陣列應用
+function App() {
+
+  const todos = ['吃早餐', '做運動', '寫程式', '玩遊戲']
+
+  return (
+    <>
+      <h1>My Todo List</h1>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+      <ul>
+        {
+          todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))
+        }
+
+      </ul>
+    </>
+  )
+}
+
+export default App
