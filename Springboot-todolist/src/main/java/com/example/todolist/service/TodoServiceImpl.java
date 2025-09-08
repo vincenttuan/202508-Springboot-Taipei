6d4f58ddf813bf73840c90737c18d5212ecfef61
teacher <teacher@DESKTOP-TDDQARH>
2025-09-08 21:04:27 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/service/TodoServiceImpl.java
+++ Springboot-todolist/src/main/java/com/example/todolist/service/TodoServiceImpl.java
@@ -1,5 +1,20 @@
 package com.example.todolist.service;
 
-public class TodoServiceImpl {
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.todolist.repository.TodoRepository;
+
+@Service
+public class TodoServiceImpl {
+	
+	@Autowired
+	private TodoRepository todoRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	
+	
 }
