48c46632b0dcc7cbe875d769e43da78efce1afcf
teacher <teacher@DESKTOP-TDDQARH>
2025-09-08 19:35:19 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/config/ModelMapperConfig.java
+++ Springboot-todolist/src/main/java/com/example/todolist/config/ModelMapperConfig.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.Scope;
 public class ModelMapperConfig {
 	
 	@Bean // 由 Spring 來管理此物件
-	@Scope("singleton") // 單一實體(預設)
+	//@Scope("singleton") // 單一實體(預設)
+	//@Scope("prototype") // 多實體
 	ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
