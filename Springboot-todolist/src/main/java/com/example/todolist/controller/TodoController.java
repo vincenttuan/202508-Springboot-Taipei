9216a05e1ab424a3047dca7df7b7017aab4c3487
teacher <teacher@DESKTOP-TDDQARH>
2025-09-08 21:32:28 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ Springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,5 +1,24 @@
 package com.example.todolist.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest API
+ * ------------------------------------
+ * Method | URI     | Message
+ * ------------------------------------
+ * GET    | "/"     | 取得所有代辦事項
+ * POST   | "/"     | 新增代辦事項
+ * PUT    | "/{id}" | 修改代辦事項
+ * DELETE | "/{id}" | 刪除代辦事項
+ * ------------------------------------
+ * */
+
+@RestController
+@RequestMapping("/todolist")
+@CrossOrigin(origins = "http://localhost:5173") // 設定跨域請求
 public class TodoController {
 
 }
