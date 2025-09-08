99935d9a130c63688d098c9807fe72937b345cf4
teacher <teacher@DESKTOP-TDDQARH>
2025-09-08 21:27:59 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/response/ApiResponse.java
+++ Springboot-todolist/src/main/java/com/example/todolist/response/ApiResponse.java
@@ -1,5 +1,15 @@
 package com.example.todolist.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+// 建立 Server 與 Client 在傳遞資料上的統一結構與標準(含錯誤)
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse<T> {
+	private boolean success;
+	private String message;
+	private T data;
 }
