53d38ff2703f3c8fb5c6109e1dab30ecec6184c2
teacher <teacher@DESKTOP-TDDQARH>
2025-09-08 21:08:04 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/service/TodoServiceImpl.java
+++ Springboot-todolist/src/main/java/com/example/todolist/service/TodoServiceImpl.java
@@ -1,19 +1,52 @@
 package com.example.todolist.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.todolist.exception.TodoNotFoundException;
+import com.example.todolist.model.dto.TodoDTO;
+import com.example.todolist.model.entity.Todo;
 import com.example.todolist.repository.TodoRepository;
 
 @Service
-public class TodoServiceImpl {
+public class TodoServiceImpl implements TodoService {
 	
 	@Autowired
 	private TodoRepository todoRepository;
 	
 	@Autowired
 	private ModelMapper modelMapper;
+
+	@Override
+	public List<TodoDTO> findAllTodos() {
+		List<Todo> todos = todoRepository.findAll();
+		List<TodoDTO> todoDTOs = todos.stream()
+									  .map(todo -> modelMapper.map(todo, TodoDTO.class))
+									  .toList();
+		return todoDTOs;
+	}
+
+	@Override
+	public TodoDTO addTodo(TodoDTO todoDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TodoDTO uptTodo(TodoDTO todoDTO) throws TodoNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delTodo(TodoDTO todoDTO) throws TodoNotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
