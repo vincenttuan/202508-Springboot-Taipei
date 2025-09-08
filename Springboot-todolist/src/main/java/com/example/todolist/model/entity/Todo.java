960b27782ee932e56a86dad3520cb51c3c46abe8
teacher <teacher@DESKTOP-TDDQARH>
2025-09-08 19:57:17 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/model/entity/Todo.java
+++ Springboot-todolist/src/main/java/com/example/todolist/model/entity/Todo.java
@@ -1,5 +1,24 @@
 package com.example.todolist.model.entity;
 
-public class Todo {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity // 對應實體資料表
+@Table(name = "todo") // 預設的資料表名
+public class Todo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 由資料庫自行決定 id 生成策略
+	private Long id;
+	
+	@Column(name = "text", length = 255, nullable = false, unique = false)
+	private String text;
+	
+	@Column(name = "completed", nullable = false, unique = false)
+	private Boolean completed;
+	
 }
