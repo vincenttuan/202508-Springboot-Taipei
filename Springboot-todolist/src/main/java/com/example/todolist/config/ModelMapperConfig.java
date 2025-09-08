46c997dd6dd6e472e227ed629be36cc435658550
teacher <teacher@DESKTOP-TDDQARH>
2025-09-08 20:49:32 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/config/ModelMapperConfig.java
+++ Springboot-todolist/src/main/java/com/example/todolist/config/ModelMapperConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import com.example.todolist.model.dto.TodoDTO;
+import com.example.todolist.model.entity.Todo;
+
 @Configuration
 public class ModelMapperConfig {
 	
@@ -12,7 +15,23 @@ public class ModelMapperConfig {
 	//@Scope("singleton") // 單一實體(預設)
 	//@Scope("prototype") // 多實體
 	ModelMapper modelMapper() {
-		return new ModelMapper();
+		ModelMapper modelMapper = new ModelMapper();
+		
+		// 自訂 Todo -> TodoDTO 欄位映射 
+		modelMapper.typeMap(Todo.class, TodoDTO.class)
+				   .addMappings(mapper -> {
+					   mapper.map(Todo::getMessage, TodoDTO::setText);
+					   mapper.map(Todo::getFinished, TodoDTO::setCompleted);
+				   });
+		
+		// 自訂 TodoDTO -> Todo 欄位映射
+		modelMapper.typeMap(TodoDTO.class, Todo.class)		   
+				   .addMappings(mapper -> {
+					   mapper.map(TodoDTO::getText, Todo::setMessage);
+					   mapper.map(TodoDTO::getCompleted, Todo::setFinished);
+				   });
+		
+		return modelMapper;
 	}
 	
 }
