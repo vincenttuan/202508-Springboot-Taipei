ee92fc5f06862e145a376c6871dc5c6c005e5630
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-12 21:17:36 +0800

    web

--- Springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ Springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.todolist.exception.TodoNotFoundException;
 import com.example.todolist.model.dto.TodoDTO;
 import com.example.todolist.response.ApiResponse;
 import com.example.todolist.service.TodoService;
@@ -37,17 +40,28 @@ public class TodoController {
 	@GetMapping(value = "/", produces = "application/json;charset=utf-8")
 	public ApiResponse<List<TodoDTO>> findAll() {
 		List<TodoDTO> todoDTOs = todoService.findAllTodos();
-		return new ApiResponse<List<TodoDTO>>(true, "查詢成功", todoDTOs);
+		return new ApiResponse<>(true, "查詢成功", todoDTOs);
 	}
 	
 	@PostMapping(value = "/", produces = "application/json;charset=utf-8")
 	public ApiResponse<TodoDTO> addTodo(@RequestBody TodoDTO todoDTO) {
 		todoDTO = todoService.addTodo(todoDTO);
-		return new ApiResponse<TodoDTO>(true, "新增成功", todoDTO);
+		return new ApiResponse<>(true, "新增成功", todoDTO);
 	}
 	
 	
-	
+	@PutMapping(value = "/{id}", produces = "application/json;charset=utf-8")
+	public ApiResponse<Object> updateTodo(@PathVariable Long id,  @RequestBody TodoDTO todoDTO) {
+		
+		try {
+			todoDTO = todoService.uptTodo(todoDTO);
+		} catch (TodoNotFoundException e) {
+			return new ApiResponse<>(false, "修改失敗", e.getMessage());
+		}
+		
+		return new ApiResponse<>(true, "修改成功", todoDTO);
+		
+	}
 	
 	
 	
