78aaf9ede1008f0a94175b65cf7d01e3e1d25e9d
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-15 19:07:12 +0800

    react

--- my-todolist-react/src/api_services/todo_services.js
+++ my-todolist-react/src/api_services/todo_services.js
@@ -11,7 +11,12 @@ const BASE_URL = 'localhost:8080/todolist';
 
 // 取得所有代辦事項
 export const fetchTodos = async() => {
-
+    const response = await fetch(BASE_URL);
+    const result = await response.json();
+    if(result.success) {
+        return result.data; // 返回所有資料紀錄
+    }
+    throw new Error(result.message);
 };
 
 // 新增代辦事項
@@ -27,5 +32,5 @@ export const updateTodo = async(updateTodo) => {
 
 // 刪除指定代辦事項
 export const deleteTodo = async(id) => {
-    
+
 };
