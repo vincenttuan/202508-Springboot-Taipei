24b5973d0be87101a29c68b1cb8cead79f2e382e
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-15 18:59:50 +0800

    react

---
+++ my-todolist-react/src/api_services/todo_services.js
@@ -0,0 +1,9 @@
+/**
+ * WEB API TodoList REST CRUD
+ * ------------------------------------------------------
+ * GET     localhost:8080/todolist/      取得所有代辦事項
+ * POST    localhost:8080/todolist/      新增代辦事項
+ * PUT     localhost:8080/todolist/{id}  修改指定代辦事項
+ * DELETE  localhost:8080/todolist/{id}  刪除指定代辦事項
+ * ------------------------------------------------------
+ */
