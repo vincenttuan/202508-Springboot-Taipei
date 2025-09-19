1bcb2ad3e1fdc31a0d2084a749448c1cc412a664
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-19 19:50:53 +0800

    web

---
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.cart.repository;
+
+import java.lang.foreign.Linker.Option;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.cart.model.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	// 自訂方法
+	Optional<User> findByUsername(String username);
+}
