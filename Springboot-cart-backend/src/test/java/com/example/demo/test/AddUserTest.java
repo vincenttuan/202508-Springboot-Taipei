a0c146f408bdb8963e70903e1f367a2bb5278788
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-19 19:53:28 +0800

    web

--- Springboot-cart-backend/src/test/java/com/example/demo/test/AddUserTest.java
+++ Springboot-cart-backend/src/test/java/com/example/demo/test/AddUserTest.java
@@ -1,10 +1,33 @@
 package com.example.demo.test;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.model.entity.User;
+import com.example.demo.cart.repository.UserRepository;
 
 @SpringBootTest
 public class AddUserTest {
 	
+	@Autowired
+	UserRepository userRepository;
 	
+	@Test
+	void add() {
+		User user1 = new User();
+		user1.setUsername("admin");
+		user1.setPassword("1234");
+		// 儲存
+		userRepository.save(user1);
+		
+		User user2 = new User();
+		user2.setUsername("mary");
+		user2.setPassword("5678");
+		// 儲存
+		userRepository.save(user2);
+		
+		
+	}
 	
 }
