839794fe68d352ff6d2c379464ebd78d01a796fe
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-19 19:27:12 +0800

    web

---
+++ Springboot-cart-backend/src/main/java/com/example/demo/SpringbootCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCartBackendApplication.class, args);
+	}
+
+}
