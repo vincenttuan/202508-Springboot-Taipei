55d9fa228e047b2117f09d55e32ac45312bad060
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-19 21:16:54 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/ProductImage.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/ProductImage.java
@@ -1,5 +1,26 @@
 package com.example.demo.cart.model.entity;
 
-public class ProductImage {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "product_image")
+public class ProductImage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// LONGTEXT = 長文本
+	@Column(columnDefinition = "LONGTEXT")
+	private String imageBase64;
+	
+	
 }
