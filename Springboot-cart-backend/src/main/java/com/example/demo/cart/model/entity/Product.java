cd231d3532b2bf5e5d4be1004cf9b716894ba487
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-19 21:11:50 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Product.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Product.java
@@ -1,5 +1,28 @@
 package com.example.demo.cart.model.entity;
 
-public class Product {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private Integer price;
+	
+	
 }
