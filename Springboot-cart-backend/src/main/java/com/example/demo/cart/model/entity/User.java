562088bc5ced68a3b58056f191005f15199e50b7
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-22 21:23:13 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/User.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/User.java
@@ -1,10 +1,13 @@
 package com.example.demo.cart.model.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,5 +32,7 @@ public class User {
 	@Column(nullable = false, length = 255)
 	private String password;
 	
+	@OneToMany(mappedBy = "user")
+	private List<Order> orders;
 	
 }
