b91275b381481347a8562d988e25025abb6e6fb9
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-22 20:02:20 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Order.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/Order.java
@@ -1,5 +1,30 @@
 package com.example.demo.cart.model.entity;
 
-public class Order {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "`order`")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// order 與 user 的關係是多對一
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	
+	
 }
