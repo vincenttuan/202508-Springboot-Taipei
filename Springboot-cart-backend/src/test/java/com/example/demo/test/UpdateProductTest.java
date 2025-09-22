c798768726ac4690af6d8c94d5cc32245ac89899
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-22 19:52:02 +0800

    web

--- Springboot-cart-backend/src/test/java/com/example/demo/test/UpdateProductTest.java
+++ Springboot-cart-backend/src/test/java/com/example/demo/test/UpdateProductTest.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.cart.repository.ProductRepository;
@@ -17,6 +18,7 @@ public class UpdateProductTest {
 	
 	@Test
 	@Transactional // 因為會使用到 JPA 的 @Modifying 方法, 所以要設定 @Transactional
+	@Commit // 防止 rollback
 	public void updateProduct() {
 		// 想要修改 id=1 的價格
 		Long id = 1L;
