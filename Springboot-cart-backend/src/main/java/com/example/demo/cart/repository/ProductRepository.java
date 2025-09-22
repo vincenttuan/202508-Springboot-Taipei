582d4e5f3a23eaa4fd4a48157199ab68e8baab6c
teacher <teacher@DESKTOP-A4QEQPK>
2025-09-22 19:40:47 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/repository/ProductRepository.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/repository/ProductRepository.java
@@ -1,11 +1,24 @@
 package com.example.demo.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.cart.model.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	// 自訂修改(使用 PQL)
+	@Modifying
+	@Query("update Product p set p.price = :price, p.name = :name where p.id = :id")
+	int updatePriceAndNameById(@Param("id") Long id, @Param("price") Integer price, @Param("name") String name);
+	
+	// 自訂修改(使用 T-SQL)
+	@Modifying
+	@Query(value="update product set price = :price, name = :name where id = :id", nativeQuery = true)
+	int updatePriceAndNameByIdNative(@Param("id") Long id, @Param("price") Integer price, @Param("name") String name);
 
 }
