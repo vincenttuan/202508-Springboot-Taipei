4a43e63f13920f95229200481be6c6d2a150b562
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-03 18:56:54 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/User.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/model/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.cart.model.entity;
 
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -8,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -36,4 +40,12 @@ public class User {
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private List<Order> orders;
 	
+	// 建立用戶可以關注商品的多對多關係
+	@ManyToMany
+	@JoinTable(
+		name = "user_product", // 關聯表名稱
+		joinColumns = @JoinColumn(name = "user_id"), // 用戶的主鍵
+		inverseJoinColumns = @JoinColumn(name = "product_id") // 商品的主鍵
+	)
+	private Set<Product> favoriteProducts;
 }
