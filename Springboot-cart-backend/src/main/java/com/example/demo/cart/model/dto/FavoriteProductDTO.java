2e196db1c1f22fa7e7b0914b7bc0be18f85fe8cc
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-03 19:45:59 +0800

    web

---
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/model/dto/FavoriteProductDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.cart.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FavoriteProductDTO {
+	private Long id;
+	private String name;
+}
