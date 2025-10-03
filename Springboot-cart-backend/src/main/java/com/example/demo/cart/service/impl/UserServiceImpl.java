e546972cb65a6d16ade36af2ecfaa874c882ea34
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-03 21:10:44 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/UserServiceImpl.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/UserServiceImpl.java
@@ -92,16 +92,26 @@ public class UserServiceImpl implements UserService {
 					.toList();
 	}
 
+	// 新增商品關注
 	@Override
 	public void addFavoriteProduct(Long userId, Long productId) {
-		// TODO Auto-generated method stub
-		
+		User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("查無使用者"));
+		Product product = productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException("查無商品"));
+		// 將商品加入到用戶關注清單
+		user.getFavoriteProducts().add(product);
+		// 保存
+		userRepository.save(user);
 	}
 
+	// 移除商品關注
 	@Override
 	public void removeFavoriteProduct(Long userId, Long productId) {
-		// TODO Auto-generated method stub
-		
+		User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("查無使用者"));
+		Product product = productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException("查無商品"));
+		// 將商品從用戶關注清單中移除
+		user.getFavoriteProducts().remove(product);
+		// 保存
+		userRepository.save(user);
 	}
 
 }
