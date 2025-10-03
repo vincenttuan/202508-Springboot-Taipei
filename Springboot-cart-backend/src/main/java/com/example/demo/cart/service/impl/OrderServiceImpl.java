3046ae0996fd0854cc6575b6653834649f0746bd
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-03 21:24:50 +0800

    web

---
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/OrderServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.cart.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.cart.model.dto.OrderDTO;
+import com.example.demo.cart.model.dto.OrderItemDTO;
+import com.example.demo.cart.repository.OrderItemRepository;
+import com.example.demo.cart.repository.OrderRepository;
+import com.example.demo.cart.repository.ProductRepository;
+import com.example.demo.cart.repository.UserRepository;
+import com.example.demo.cart.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Override
+	public List<OrderDTO> findOrdersByUserId(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<OrderDTO> saveOrder(Long userId, List<OrderItemDTO> items) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+}
