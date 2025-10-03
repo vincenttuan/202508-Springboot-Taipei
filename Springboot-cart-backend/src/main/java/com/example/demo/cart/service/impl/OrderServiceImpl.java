802f9f50b6c7d17b81da8909bb0a704ca179778a
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-03 21:35:35 +0800

    web

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/OrderServiceImpl.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/service/impl/OrderServiceImpl.java
@@ -11,6 +11,7 @@ import com.example.demo.cart.exception.UserNotFoundException;
 import com.example.demo.cart.model.dto.OrderDTO;
 import com.example.demo.cart.model.dto.OrderItemDTO;
 import com.example.demo.cart.model.entity.Order;
+import com.example.demo.cart.model.entity.OrderItem;
 import com.example.demo.cart.model.entity.User;
 import com.example.demo.cart.repository.OrderItemRepository;
 import com.example.demo.cart.repository.OrderRepository;
@@ -49,10 +50,25 @@ public class OrderServiceImpl implements OrderService {
 		
 		// 2. 建立訂單 
 		Order order = new Order();
+		order.setUser(user); // 設定訂單與用戶的關聯
 		
+		// 3. 建立訂單明細列表
+		List<OrderItem> orderItems = items.stream()
+				.map(item -> {
+					OrderItem orderItem = modelMapper.map(item, OrderItem.class);
+					orderItem.setOrder(order); // 設定 item 與 order 的關係
+					return orderItem;
+				})
+				.toList();
 		
+		// 4. 設定 order 與 orderItems 的關係
+		order.setOrderItems(orderItems);
 		
-		return Optional.empty();
+		// 5. 保存
+		order = orderRepository.save(order);
+		
+		OrderDTO orderDTO = modelMapper.map(order, OrderDTO.class); 
+		return Optional.of(orderDTO);
 	}
 
 }
