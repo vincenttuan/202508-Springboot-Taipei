7e18fb5f5db6ecc87a6a50f83e0ed9cc900c22d4
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-03 19:10:56 +0800

    web

---
+++ Springboot-cart-backend/src/test/java/com/example/demo/test/AddFavorite.java
@@ -0,0 +1,33 @@
+package com.example.demo.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.model.entity.Product;
+import com.example.demo.cart.model.entity.User;
+import com.example.demo.cart.repository.ProductRepository;
+import com.example.demo.cart.repository.UserRepository;
+
+@SpringBootTest
+public class AddFavorite {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	void add() {
+		// 商品
+		Product p1 = productRepository.findById(1L).get();
+		Product p2 = productRepository.findById(2L).get();
+		// 使用者
+		User user = userRepository.findById(1L).get();
+		// 使用者關注商品 -> 建立與商品之間的關聯關係
+		user.getFavoriteProducts().add(p1);
+		user.getFavoriteProducts().add(p2);
+		// 保存
+		userRepository.save(user);
+	}
+	
+}
