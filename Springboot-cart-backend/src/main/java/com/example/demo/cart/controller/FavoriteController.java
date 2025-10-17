978078f69fbf1d95604d8833b91deb549ec3a9bf
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-17 20:03:18 +0800

    cart

--- Springboot-cart-backend/src/main/java/com/example/demo/cart/controller/FavoriteController.java
+++ Springboot-cart-backend/src/main/java/com/example/demo/cart/controller/FavoriteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,14 +39,28 @@ public class FavoriteController {
 	
 	// 獲取商品被那些用戶關注清單
 	@GetMapping("/product/{productId}")
-	@CheckUserSession
 	public ResponseEntity<ApiResponse<List<FavoriteUserDTO>>> getFavoriteUsers(@PathVariable Long productId) {
 		List<FavoriteUserDTO> favoriteUserDTOs = userService.getFavoriteUsers(productId);
 		return ResponseEntity.ok(new ApiResponse<>(200, "獲取商品被那些用戶關注清單", favoriteUserDTOs));
 	}
+	
 	// 加入關注
+	@PostMapping("/productId")
+	@CheckUserSession
+	public ResponseEntity<ApiResponse<String>> addFavorite(@PathVariable Long productId, HttpSession session) {
+		Long userId = ((UserDTO)session.getAttribute("userDTO")).getId();
+		userService.addFavoriteProduct(userId, productId);
+		return ResponseEntity.ok(new ApiResponse<>(200, "加入關注成功", "商品已加入關注"));
+	}
 	
 	// 取消關注
+	@PostMapping("/productId")
+	@CheckUserSession
+	public ResponseEntity<ApiResponse<String>> removeFavorite(@PathVariable Long productId, HttpSession session) {
+		Long userId = ((UserDTO)session.getAttribute("userDTO")).getId();
+		userService.removeFavoriteProduct(userId, productId);
+		return ResponseEntity.ok(new ApiResponse<>(200, "取消關注成功", "商品已取消關注"));
+	}
 	
 	
 }
