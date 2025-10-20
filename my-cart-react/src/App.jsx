729510261442b823ed474b5977d289582b5208f5
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-20 19:14:26 +0800

    cart

--- my-cart-react/src/App.jsx
+++ my-cart-react/src/App.jsx
@@ -9,8 +9,29 @@ import './App.css'
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+// 登入相關
+import LoginPage from "./pages/LoginPage";
+import { checkLoginStatus, login, logout } from "./services/authService";
 
 function App() {
+  // 登入狀態
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 處理登入邏輯
+  const handleLogin = async (username, password) => {
+    try {
+      const data = await login(username, password); // 使用登入服務方法
+      if(data.message === "登入成功") {
+        setIsLoggedIn(true); // 修改登入狀態
+        window.location.href = "/"; // 回到首頁
+      } else {
+        alert("登入失敗");
+      }
+    } catch(e) {
+      console.error("登入錯誤:", e);
+    }
+  };
+
   return (
     <Router>
       <Navbar />
