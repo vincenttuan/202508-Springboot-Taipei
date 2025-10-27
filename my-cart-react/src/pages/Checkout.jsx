0476e067d3d88b4799d5e1af76e6270bbce5866c
teacher <teacher@DESKTOP-A4QEQPK>
2025-10-27 19:39:03 +0800

    cart

--- my-cart-react/src/pages/Checkout.jsx
+++ my-cart-react/src/pages/Checkout.jsx
@@ -28,10 +28,11 @@ function Checkout() {
       <p className="checkout-message">感謝您的購買！您的訂單已成功提交。</p>
 
       <h2 className="order-history-title">歷史訂單紀錄</h2>
+      
       {loading && <p className="loading-message">載入中...</p>}
-      {!loading && orderHistory.length === 0 && (
-        <p className="no-orders-message">目前沒有歷史訂單。</p>
-      )}
+      
+      {!loading && orderHistory.length === 0 && (<p className="no-orders-message">目前沒有歷史訂單。</p>)}
+      
       {!loading && orderHistory.length > 0 && (
         <ul className="order-list">
           {orderHistory.map((order, index) => {
@@ -46,12 +47,12 @@ function Checkout() {
             return (
               <li key={index} className="order-item">
                 <h3 className="order-summary">
-                  訂單 #{index + 1} (訂單 ID: {order.orderUUID}, 總價: ${totalAmount})
+                  序號 #{index + 1} (訂單 ID: {order.id}, 總價: ${totalAmount})
                 </h3>
                 <ul className="order-items-list">
                   {order.orderItems.map((item, itemIndex) => (
                     <li key={itemIndex} className="order-item-detail">
-                      <img src={item.product.imageBase64} alt={item.product.name} width="100" valign="middle" /> {item.product.name} - ${item.product.price} - 數量: {item.quantity}
+                      <img src={item.product.imageBase64} alt={item.product.name} width="100" valign="middle" /> {item.product.name} - ${item.product.price} - 數量: {item.qty}
                     </li>
                   ))}
                 </ul>
